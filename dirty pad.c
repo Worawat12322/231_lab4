3a0acb55c59920b66a3ec8eb20de68426e4d85cd
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-04 14:48:57 +0700

    Update dirty pad.c

--- dirty pad.c
+++ dirty pad.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	char que[200];
+	int chk,i;
+	char que[200],c_ch;
 	gets(que);
-	
+	chk=strlen(que);
+	c_ch=que[0];
+	printf("%c",que[0]);
+	for(i=0;i<chk;i++){
+		if(c_ch!=que[i+1])
+		{
+			
+		}
+	}
 	return 0;
 }
