9d9ab5e7e61d188c7feddc7ebf60c9ea09e0be06
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-04 15:10:40 +0700

    update dp c++

--- dirty pad C++.cpp
+++ dirty pad C++.cpp
@@ -1,5 +1,24 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-	
+	int chk,i,l;
+	char que[200],c_ch;
+	for(l=0;l<200;l++){
+		scanf("%c",&que[l]);
+		if(que[l]==32)
+		{
+			return 0;
+		}
+	}
+	chk=strlen(que);
+	c_ch=que[0];
+	printf("%c",que[0]);
+	for(i=0;i<chk-1;i++){
+		if(c_ch!=que[i+1])
+		{
+			c_ch=que[i+1];
+			printf("%c",c_ch);
+		}
+	}
 	return 0;
 }
