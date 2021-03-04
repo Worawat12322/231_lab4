884b8ee239a08d4a9d408902bd3252b0f99c64cb
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-04 16:51:43 +0700

    update fg

--- Four glass.cpp
+++ Four glass.cpp
@@ -3,6 +3,7 @@
 int main(){
 	int shuff[2][2]={{1,2},{3,4}};
 	int num,i,l,tmp;
+	int a,b;
 	scanf("%d",&num);
 	char move[num][50];
 	if(num>0&&num<100)
@@ -14,42 +15,49 @@ int main(){
 			scanf("%c",&move[i][l]);
 		}
 	}
-	int a,b;
+
 	for(a=0;a<num;a++)
 	{
 		for(b=0;b<strlen(move[a]);b++)
 		{
-			switch(move[a][b])
+			if(move[a][b]=='A')
 			{
-				case 'A':	tmp = shuff[1][0];
+				tmp = shuff[1][0];
 						 	shuff[1][0] = shuff[0][1];
 						 	shuff[0][1] = tmp;
-			    	break;
-			    case 'B':	tmp = shuff[0][0];
+			}
+			else if(move[a][b]=='B')
+			{
+					tmp = shuff[0][0];
 						 	shuff[0][0] = shuff[1][1];
 						 	shuff[1][1] = tmp;
-					break;
-				case 'C':	tmp = shuff[0][0];
+			}
+			else if(move[a][b]=='C')
+			{
+				tmp = shuff[0][0];
 						 	shuff[0][0] = shuff[1][0];
 						 	shuff[1][0] = tmp;
 						 	tmp = shuff[0][1];
 						 	shuff[0][1] = shuff[1][1];
 						 	shuff[1][1] = tmp;
-					break;
-				case 'D':	tmp = shuff[0][0];
+			}
+			else if(move[a][b]=='D')
+			{
+					tmp = shuff[0][0];
 						 	shuff[0][0] = shuff[0][1];
 						 	shuff[0][1] = tmp;
 						 	tmp = shuff[1][0];
 						 	shuff[1][0] = shuff[1][1];
 						 	shuff[1][1] = tmp;
-					break;
-				case 'E':	tmp = shuff[0][0];
+			}
+			else if(move[a][b]=='E')
+			{
+					tmp = shuff[0][0];
 						 	shuff[0][0] = shuff[1][1];
 						 	shuff[1][1] = tmp;
 						 	tmp = shuff[0][1];
 						 	shuff[0][1] = shuff[1][0];
 						 	shuff[1][0] = tmp;
-					break;
 			}
 		}
 		printf("%d\n",shuff[0][0]);
